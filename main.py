87d700c6497c444780cd64644afc4243719dfe06
itvatis <82761291+itvatis@users.noreply.github.com>
2024-08-20 08:23:09 +0100

    Added sorting by character count and printing report

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ def main():
     text = get_book_text(path_to_file)
     word_count = count_words(text)
     lowered_text = text.lower()
-    print(f"Word count: {word_count}")
+    print("--- Begin report of books/frankenstein.txt --- \n")
+    print(f"{word_count} words found in the document\n")
     letter_counts = count_letters(lowered_text)
-    for letter, count in letter_counts.items():
-        print(f"'{letter}': {count}")
+    # Assuming letter_counts is a list of dictionaries like [{'h': 1}, {'e': 1}, ...]
+    letter_counts.sort(reverse=True, key=sort_on)
+    for letter_count in letter_counts:
+        for letter, count in letter_count.items():
+            print(f"The character '{letter}' was found {count} times in the document")
+    print("--- End report ---")
+
 
    
 def count_words(text):
@@ -24,6 +30,14 @@ def count_letters(lowered_text):
             charactercount[i] += 1
         else:
             charactercount[i] = 1
-    return charactercount
+    
+    # Generate a list of dictionaries
+    list_of_dicts = [{letter: count} for letter, count in charactercount.items()]
+    
+    return list_of_dicts
+
+def sort_on(letter_count_dict):
+    # Extract the single value from the dictionary, which is the count
+    return next(iter(letter_count_dict.values()))
 
 main()
